b2a1f0f315a46695871c6d26c0ca29e91d5f5410
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-08-14 22:53:34 -0400

    Specs for Task Velocity

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,16 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Task do
-  let(:task) { Task.new }
+  describe "initialize" do
+    let(:task) { Task.new }
 
-  it "does not have a new task as complete" do
-    expect(task).to_not be_complete
+    it "does not have a new task as complete" do
+      expect(task).to_not be_complete
+    end
+
+    it "should allow to complete task" do
+      task.mark_completed
+      expect(task).to be_complete
+    end
   end
 
-  it "should allow to complete task" do
-    task.mark_completed
-    expect(task).to be_complete
-  end
+  describe "velocity" do
+    let(:task) { Task.new(size: 3) }
 
+    it "does not count imcomplete task towards velocity" do
+      expect(task).not_to be_complete
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eql(0)
+    end
+
+    it "does count recently completed task towards velocity" do
+      task.mark_completed(1.day.ago)
+      expect(task).to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eql(3)
+    end
+
+    it "does not count long completed task towards velocity" do
+      task.mark_completed(6.months.ago)
+      expect(task).not_to be_a_part_of_velocity
+      expect(task.points_toward_velocity).to eql(0)
+    end
+  end
 end
-
